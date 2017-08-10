4ac4b151df7e038ccb6a70f5e01997f04837057b
庄元丰 <1127848112@qq.com>
2017-08-10 09:34:36 +0800

    day.2

--- springMVC/README.md
+++ springMVC/README.md
@@ -1,3 +1,4 @@
 springMVC的基本形状   
 不知道为什么 spring 4.3.9自己冲突了 我的环境是jdk1.8 tomcat9 ，  8也试了。。   
 ***
+day 2 ..springMVC的MVC
---
+++ 分析文件夹结构/A/FileResolver.java
@@ -0,0 +1,44 @@
+package A;
+
+import java.io.File;
+
+/*
+ * 解析路径列表
+ */
+public class FileResolver {
+	
+	static int depth = 0;
+	public void parseFile(File file){
+		printFile(file , depth);
+		if(file.isFile()){
+			printFile(file , depth);
+			return;
+		}else if(file.isDirectory()){
+			depth++;
+			File[] files = file.listFiles();
+			if(files == null ){
+				return ;
+			}
+			for(File f : files){
+				parseFile(f);
+			}
+			printFile(file , depth);
+			depth--;
+		}else{
+			throw new RuntimeException("该文件既不是普通文件也不是路径");
+		}
+	}
+	
+	public void printFile(File file , int depth){
+		for(int i = 0 ;i < depth ; i++){
+			System.out.print("   ");		
+			System.out.print("|");
+		}
+		System.out.println(file.getName());
+	}
+	
+	public static void main(String[] args) {
+		File file = new File("D:/phpStudy/WWW/wuliu2/wuliu/public");
+		new FileResolver().parseFile(file);
+	}
+}
